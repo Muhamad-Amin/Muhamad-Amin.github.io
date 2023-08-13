7bd21833f20a462148fb73fdabfc04dca4a1dafa
Muhamad-Amin <terbarubug7@gmail.com>
2023-08-13 14:08:06 +0700

    update portfolio tailwind

---
+++ dist/js/script.js
@@ -0,0 +1,63 @@
+// alert('Website ini belum disempurnakan jadi maaf jika ada bug dan mohon jangan menggunakan darkmode')
+
+// Navbar fixed
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    toTop = document.querySelector('#to-top');
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
+    } else {
+        header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
+    }
+}
+
+
+// Hamburger
+
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+})
+
+
+
+// Klik di luar Hamburger
+window.addEventListener('click', function (e) {
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+
+// Darkmode Toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function (e) {
+    if (darkToggle.checked) {
+        // html.classList.add('dark');
+        alert('Mohon Maaf Darkmode belum bisa digunakan')
+        // localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+
+// Pindahkan Posisi Toogle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+} else {
+    darkToggle.checked = false;
+}
